0d3a2a5ae5834b9a3b4a309d2d39a0bf4c68bed3
Dan Phung <leftbyte@gmail.com>
2013-11-15 07:28:21 +0000

    Add actual google map.

--- googlemaps/waypoints/views.py
+++ googlemaps/waypoints/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello')
+    'Display map'
+    return render_to_response('waypoints/index.html', {
+    })
