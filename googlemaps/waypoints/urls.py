0d437c1f83c339fd4cd271643802831a1b55b2a4
Dan Phung <leftbyte@gmail.com>
2013-11-16 09:53:06 +0000

    Added search and rank by distance to searched waypoint.

--- googlemaps/waypoints/urls.py
+++ googlemaps/waypoints/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import *
 urlpatterns = patterns('googlemaps.waypoints.views',
     url(r'^$', 'index', name='waypoints-index'),
     url(r'^save$', 'save', name='waypoints-save'),
+    url(r'^search$', 'search', name='waypoints-search'),
 )
--- googlemaps/waypoints/views.py
+++ googlemaps/waypoints/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 from googlemaps.waypoints.models import Waypoint
+from django.contrib.gis.geos import Point
 import simplejson
 
 def index(request):
@@ -21,3 +22,26 @@ def save(request):
         waypoint.geometry.set_y(float(waypointY))
         waypoint.save()
     return HttpResponse(simplejson.dumps(dict(isOk=1)), mimetype='application/json')
+
+def search(request):
+    'Search/Rank waypoints'
+    # Build searchPoint
+    try:
+        searchPoint = Point(float(request.GET.get('lng')), float(request.GET.get('lat')))
+    except:
+        return HttpResponse(simplejson.dumps(dict(isOk=0, message='Could not parse search point')))
+
+    # Search database
+    waypoints = Waypoint.objects.distance(searchPoint).order_by('distance')
+    return HttpResponse(simplejson.dumps(dict(
+                isOk=1,
+                content=render_to_string('waypoints/waypoints.html', {
+                        'waypoints': waypoints
+                        }),
+                waypointByID=dict((x.id, {
+                            'name': x.name,
+                            'lat' : x.geometry.y,
+                            'lng' : x.geometry.x,
+                            }) for x in waypoints),
+                )), mimetype='application/json')
+
