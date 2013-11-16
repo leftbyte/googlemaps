d2a0ff26601bb007f8e6522c5a4bfb27bdabf56b
Dan Phung <leftbyte@gmail.com>
2013-11-16 09:09:37 +0000

    Added waypoints view.  Had to recreate the DB for this to work.

--- googlemaps/waypoints/models.py
+++ googlemaps/waypoints/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 from django.contrib.gis.db import models
 
-
 class Waypoint(models.Model):
-
+    def __unicode__(self):
+        return '%s %s %s' % (self.name, self.geometry.x, self.geometry.y)
     name = models.CharField(max_length=32)
     geometry = models.PointField(srid=4326)
     objects = models.GeoManager()
-
-    def __unicode__(self):
-        return '%s %s %s' % (self.name, self.geometry.x, self.geometry.y)
--- googlemaps/waypoints/views.py
+++ googlemaps/waypoints/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render_to_response
+from django.template.loader import render_to_string
+# Import custom modules
+from googlemaps.waypoints.models import Waypoint
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
     'Display map'
+    waypoints = Waypoint.objects.order_by('name')
     return render_to_response('waypoints/index.html', {
+        'waypoints': waypoints,
+        'content': render_to_string('waypoints/waypoints.html', {'waypoints': waypoints}),
     })
